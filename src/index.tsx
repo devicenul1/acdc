81da31b12c5fe645ef1bcd1702ad63c0a5073464
Burress, William <william.burress@egov.com>
2021-10-01 11:24:57 -0400

    service worker fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import classes from 'index.module.scss';
-import * as serviceWorkerRegistration from 'elements/serviceWorkerRegistration';
+import * as serviceWorkerRegistration from 'serviceWorkerRegistration';
 // import reportWebVitals from 'reportWebVitals';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
